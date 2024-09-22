62cdd300b7c22e3321159ff61d9cf61a2f0f5498
saudenaa <saudenaa@mail.uc.edu>
2024-09-21 22:49:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // hardcoded value for the integer to examin
+        int numToExamin = 2;
+
+        //check if the number is even or odd using the modulo operator
+
+        if (numToExamin % 2 == 0 ){
+            System.out.println(numToExamin + " is even .");
+        }
+        else{
+        System.out.println(numToExamin + " is odd .");
+        }
+
+
+    }
+}
